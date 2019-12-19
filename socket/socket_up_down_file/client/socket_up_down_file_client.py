133d917b126516c22997fa6915274db30da8a452
panxiong <1357211280@qq.com>
2019-12-19 11:36:16 +0800

    用socket完成文件的上传与下载功能

---
+++ socket/socket_up_down_file/client/socket_up_down_file_client.py
@@ -0,0 +1,128 @@
+# encoding: utf-8
+
+import socket
+import struct
+import json
+import os
+
+
+class MYTCPClient:
+    address_family = socket.AF_INET        # 地址族
+    socket_type = socket.SOCK_STREAM       # socket类型
+    allow_reuse_address = False            # 是否允许地址端口重用
+    max_packet_size = 8192                 # 每次接收包的最大大小
+    coding = 'utf-8'
+    request_queue_size = 5                 # 监听的最在数量
+    header_len = 4                         # 报头头部固定长度
+
+    def __init__(self, server_address, connect=True):
+        self.server_address = server_address
+        self.socket = socket.socket(self.address_family, self.socket_type)
+        if connect:
+            try:
+                self.connect()
+            except:
+                self.close()
+                raise
+
+    def connect(self):
+        self.socket.connect(self.server_address)
+
+    def close(self):
+        self.socket.close()
+
+    def make_header_and_send(self, header_dict):
+        """ 制作报头并发送，需要一个报头的dict存储报头信息"""
+        print(header_dict)
+        head_json = json.dumps(header_dict)
+        head_json_bytes = bytes(head_json, encoding=self.coding)
+        head_stuct = struct.pack('i', len(head_json_bytes))
+        self.socket.send(head_stuct)
+        self.socket.send(head_json_bytes)
+
+    def recv_header(self):
+        """接收头部信息"""
+        header_struct = self.socket.recv(self.header_len)
+        head_len = struct.unpack('i', header_struct)[0]
+        head_json = self.socket.recv(head_len).decode('utf-8')
+        head_dict = json.loads(head_json)
+        return head_dict
+
+    def recv_body(self, head_dict):
+        """接收下载的真实的数据"""
+        filesize = head_dict['filesize']
+        filename = head_dict['filename']
+
+        with open(filename, 'wb') as f:
+            recv_size = 0
+            while recv_size < filesize:
+                line = self.socket.recv(self.max_packet_size)
+                f.write(line)
+                recv_size += len(line)
+
+    def run(self):
+        while True:
+            inp = input(">>: ").strip()  # 命令的形式"get a.txt.txt" 或者"put a.txt.txt"
+            if not inp:
+                continue
+            inp_list = inp.split()
+            cmd = inp_list[0]
+            print(cmd)
+            if hasattr(self, cmd):
+                func = getattr(self, cmd)
+                func(inp_list)
+
+    def put(self, args):
+        """上传文件数据的操作"""
+        cmd = args[0]
+        filename = args[1]
+        if not os.path.isfile(filename):
+            print('file:{} is not exists'.format(filename))
+            return
+        else:
+            filesize = os.path.getsize(filename)
+
+        # 1.制作上传的报头信息
+        header_dict = {
+            'cmd': cmd,
+            'filename': os.path.basename(filename),
+            'md5': 'xxxxx',
+            'filesize': filesize
+        }
+        self.make_header_and_send(header_dict)
+
+        # 2.上传真实的数据
+        send_size = 0
+        with open(filename, 'rb') as f:
+            for line in f:
+                self.socket.send(line)
+                send_size += len(line)
+                print(send_size)
+            else:
+                print('upload successful')
+
+    def get(self, args):
+        """下载文件的操作"""
+        # 制作报头dict
+        header_dict = {
+            'cmd': args[0],
+            'filename': args[1],
+        }
+        # 1.发送下载的命令与文件名
+        self.make_header_and_send(header_dict)
+
+        # 2.接收头部, 获取报头信息
+        head_dict = self.recv_header()
+        print(head_dict)
+
+        # 3.接收真实的数据
+        self.recv_body(head_dict)
+
+
+def main():
+    client = MYTCPClient(('127.0.0.1', 8080), connect=True)
+    client.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ socket/socket_up_down_file/server/socket_up_down_file_server.py
@@ -0,0 +1,149 @@
+# encoding: utf-8
+import socket
+import struct
+import json
+import os
+import threading
+
+
+class MYTCPServer:
+    address_family = socket.AF_INET
+    socket_type = socket.SOCK_STREAM
+    allow_reuse_address = False
+    max_packet_size = 8192
+    coding = 'utf-8'
+    request_queue_size = 5
+    server_dir = os.path.join(os.path.dirname(__file__), 'file_upload')
+
+    def __init__(self, server_address, bind_and_activate=True):
+        self.server_address = server_address
+        self.socket = socket.socket(self.address_family, self.socket_type)
+        if bind_and_activate:
+            try:
+                self.bind()
+                self.activate()
+            except:
+                self.close()
+                raise
+
+    def bind(self):
+        if self.allow_reuse_address:
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind(self.server_address)
+        self.server_address = self.socket.getsockname()
+
+    def activate(self):
+        self.socket.listen(self.request_queue_size)
+
+    def close(self):
+        self.socket.close()
+
+    def get_request(self):
+        return self.socket.accept()
+
+    def close_request(self, request):
+        request.close()
+
+    def run(self):
+        while True:
+            conn, client_addr = self.get_request()
+            print('from client', client_addr)
+            socket_handle = MySocketHandle(conn, client_addr)
+
+            # 用线程来处理每一个连接
+            client_thread = threading.Thread(target=socket_handle.handle_sock, args=())
+            client_thread.start()
+
+
+class MySocketHandle(object):
+    max_packet_size = 8192
+    coding = 'utf-8'
+    header_len = 4  # 报头头部固定长度
+    server_dir = os.path.join(os.path.dirname(__file__), 'file_upload')
+
+    def __init__(self, socket, client_addr):
+        self.socket = socket
+        self.client_addr = client_addr
+
+    def make_header_and_send(self, header_dict):
+        """ 制作报头并发送，需要一个报头的dict存储报头信息"""
+        print("发送数据的报头", header_dict)
+        head_json = json.dumps(header_dict)
+        head_json_bytes = bytes(head_json, encoding=self.coding)
+        head_stuct = struct.pack('i', len(head_json_bytes))
+        self.socket.send(head_stuct)
+        self.socket.send(head_json_bytes)
+
+    def recv_header(self):
+        """接收头部信息"""
+        header_struct = self.socket.recv(self.header_len)
+        head_len = struct.unpack('i', header_struct)[0]
+        head_json = self.socket.recv(head_len).decode('utf-8')
+        head_dict = json.loads(head_json)
+        return head_dict
+
+    def handle_sock(self):
+        print('from client', self.client_addr)
+        while True:
+            try:
+                head_dict = self.recv_header()
+                print("接收的命令报头：", head_dict)
+                cmd = head_dict['cmd']
+                if hasattr(self, cmd):
+                    func = getattr(self, cmd)
+                    func(head_dict)
+            except Exception:
+                break
+
+    def put(self, head_dict):
+        """上传文件的操作"""
+        file_path = os.path.normpath(os.path.join(self.server_dir, head_dict['filename']))
+        filesize = head_dict['filesize']
+        recv_size = 0
+        print('----->', file_path)
+        with open(file_path, 'wb') as f:
+            while recv_size < filesize:
+                recv_data = self.socket.recv(self.max_packet_size)
+                f.write(recv_data)
+                recv_size += len(recv_data)
+                print('recvsize:{} filesize {}'.format(recv_size, filesize))
+
+    def get(self, head_dict):
+        """下载文件的操作"""
+        filename = head_dict['filename']
+        file_path = os.path.normpath(os.path.join(self.server_dir, filename))
+        if not os.path.isfile(file_path):
+            print('file:{} is not exists'.format(file_path))
+            return
+        else:
+            filesize = os.path.getsize(file_path)
+
+        header_dict = {
+            'filename': filename,
+            'md5': 'xxxxx',
+            'filesize': filesize
+        }
+        # 发送下载的头部
+        self.make_header_and_send(header_dict)
+
+        # 发送真实的文件数据
+        send_size = 0
+        with open(file_path, 'rb') as f:
+            for line in f:
+                self.socket.send(line)
+                send_size += len(line)
+                print("总数据：{}------已发送：{}".format(filesize, send_size))
+            else:
+                print('download successful')
+
+    def close(self):
+        self.socket.close()
+
+
+def main():
+    tcpserver = MYTCPServer(('127.0.0.1', 8080), bind_and_activate=True)
+    tcpserver.run()
+
+
+if __name__ == "__main__":
+    main()
